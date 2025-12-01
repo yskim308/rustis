b3b3fee47d2bb8b1354653eaeb2db9e1d4257539
Young Seo Kim <youngseokim7@gmail.com>
2025-12-01 19:36:51 +0900

    intiial skeleton for parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,1 +1,30 @@
-pub struct Parser {}
+pub struct Parser {
+    buffer: Vec<u8>,
+    cursor: usize,
+}
+
+impl Parser {
+    fn peek(&self) -> u8 {
+        self.buffer[self.cursor]
+    }
+
+    // tood: make anenum for value, return that enum in parse
+    // todo: proper error handling, return result on parse
+    fn parse(&mut self) {
+        match self.peek() {
+            b'+' => self.parse_simple_string(),
+            b'-' => self.parse_simple_error(),
+            b':' => self.parse_integer(),
+            b'$' => self.parse_bulk_string(),
+            _ => panic!("failed parsing"),
+        }
+    }
+
+    fn parse_simple_string(&mut self) {}
+
+    fn parse_simple_error(&mut self) {}
+
+    fn parse_integer(&mut self) {}
+
+    fn parse_bulk_string(&mut self) {}
+}
