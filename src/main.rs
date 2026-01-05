8698d77634cefc1151437ef04d5066f6675db1ed
Young Seo Kim <youngseokim7@gmail.com>
2026-01-05 19:54:24 +0900

    initialize parser in main, pass in vector and accumulate reads if necessary

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
+use rustis::parser::{BufParseError, Parser};
+
 fn main() {
     let port = 4000;
     let addr = format!("127.0.0.1:{}", port);
@@ -23,15 +25,29 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
+    let mut accumulated: Vec<u8> = Vec::new();
     loop {
         match stream.read(&mut buffer) {
             Ok(0) => {
                 break;
             }
-            Ok(bytes) => {
-                if let Err(e) = stream.write_all(&buffer[..bytes]) {
-                    eprintln!("failed to write to stream: {}", e);
-                    break;
+            Ok(bytes_read) => {
+                // todo: not sure if we should be creating a new parser instance per read
+                accumulated.extend_from_slice(&buffer[..bytes_read]);
+                let mut parser = Parser::new(&accumulated);
+
+                match parser.parse() {
+                    Ok(value) => {
+                        println!("{value:?}");
+                        accumulated.clear();
+                    }
+                    Err(BufParseError::Incomplete) => {
+                        continue;
+                    }
+                    Err(e) => {
+                        eprint!("parse error: {:?}", e);
+                        break;
+                    }
                 }
             }
             Err(e) => {
