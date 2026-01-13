0b722f4c126847b6a19237b58e6c959d9ac52626
Young Seo Kim <youngseokim7@gmail.com>
2026-01-13 10:59:07 +0900

    implement proper error handling, avoid unwraps

--- src/kv.rs
+++ src/kv.rs
@@ -2,6 +2,10 @@ use bytes::Bytes;
 use std::collections::HashMap;
 use std::sync::{Arc, RwLock};
 
+#[derive(Debug)]
+enum DatabaseError {
+    PoisonedLock,
+}
 /// We use Arc to share it across connection tasks and RwLock to allow
 /// concurrent reads but exclusive writes.
 #[derive(Clone, Debug)]
@@ -24,21 +28,22 @@ impl KvStore {
     }
 
     /// Sets a value in the store.
-    pub fn set(&self, key: String, value: Bytes) {
-        let mut db = self.db.write().unwrap();
+    pub fn set(&self, key: String, value: Bytes) -> Result<(), DatabaseError> {
+        let mut db = self.db.write().map_err(|_| DatabaseError::PoisonedLock)?;
         db.insert(key, value);
+        Ok(())
     }
 
     /// Gets a value from the store.
     /// Returns None if the key does not exist.
-    pub fn get(&self, key: &str) -> Option<Bytes> {
-        let db = self.db.read().unwrap();
-        db.get(key).cloned() // Cloning Bytes is O(1)
+    pub fn get(&self, key: &str) -> Result<Option<Bytes>, DatabaseError> {
+        let db = self.db.read().map_err(|_| DatabaseError::PoisonedLock)?;
+        Ok(db.get(key).cloned()) // Cloning Bytes is O(1)
     }
 
     /// Removes a key from the store.
-    pub fn del(&self, key: &str) -> bool {
-        let mut db = self.db.write().unwrap();
-        db.remove(key).is_some()
+    pub fn del(&self, key: &str) -> Result<bool, DatabaseError> {
+        let mut db = self.db.write().map_err(|_| DatabaseError::PoisonedLock)?;
+        Ok(db.remove(key).is_some())
     }
 }
