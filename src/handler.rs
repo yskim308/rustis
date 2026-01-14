0bf9fbc3e214ce7a35490b6eec411c56767a8192
Young Seo Kim <youngseokim7@gmail.com>
2026-01-14 17:51:00 +0900

    implement lrange

--- src/handler.rs
+++ src/handler.rs
@@ -1,12 +1,23 @@
 use bytes::Bytes;
 use std::sync::Arc;
-use tokio::task::coop::RestoreOnPending;
 
 use crate::kv::{KvStore, RedisValue};
 use crate::parser::ReponseValue;
 
 pub struct CommandHandler {
     kv: Arc<KvStore>,
+}
+
+fn parse_int(value: &ReponseValue) -> Result<i64, String> {
+    match value {
+        ReponseValue::BulkString(Some(bytes)) => {
+            let s = std::str::from_utf8(bytes)
+                .map_err(|_| "ERR value is not valid utf8".to_string())?;
+            s.parse::<i64>()
+                .map_err(|_| "ERR value is not an integer or out of range".to_string())
+        }
+        _ => Err("ERR protocol error: expected bulk string".to_string()),
+    }
 }
 
 impl CommandHandler {
@@ -37,6 +48,7 @@ impl CommandHandler {
             "SET" => self.handle_set(args),
             "LPUSH" => self.handle_lpush(args),
             "RPUSH" => self.handle_rpush(args),
+            "LRANGE" => self.handle_lrange(args),
             _ => ReponseValue::Error("invalid command".to_string()),
         }
     }
@@ -127,6 +139,36 @@ impl CommandHandler {
         match self.kv.rpush(key.to_string(), values) {
             Ok(size) => ReponseValue::Integer(size),
             Err(err) => ReponseValue::Error(format!("internal db error: {:?}", err)),
+        }
+    }
+
+    fn handle_lrange(&self, args: &[ReponseValue]) -> ReponseValue {
+        let key = match &args[0] {
+            ReponseValue::BulkString(Some(bytes)) => String::from_utf8_lossy(bytes),
+            _ => return ReponseValue::Error("key must be bulk string".to_string()),
+        };
+
+        let start = match parse_int(&args[1]) {
+            Ok(integer) => integer,
+            Err(err) => return ReponseValue::Error(err),
+        };
+
+        let stop = match parse_int(&args[2]) {
+            Ok(integer) => integer,
+            Err(err) => return ReponseValue::Error(err),
+        };
+
+        match self.kv.lrange(&key, start, stop) {
+            Ok(bytes_vec) => {
+                // Convert Vec<Bytes> -> Vec<ReponseValue>
+                let response_elements: Vec<ReponseValue> = bytes_vec
+                    .into_iter()
+                    .map(|b| ReponseValue::BulkString(Some(b.to_vec())))
+                    .collect();
+
+                ReponseValue::Array(Some(response_elements))
+            }
+            Err(err) => ReponseValue::Error(format!("ERR {:?}", err)),
         }
     }
 }
