4f50f2706e073053a422f5f21f3ead9fb9e23509
Young Seo Kim <youngseokim7@gmail.com>
2026-02-17 21:52:16 +0900

    refactor of worker into using a struct with future implementation

--- src/worker.rs
+++ src/worker.rs
@@ -1,41 +1,99 @@
-use rtrb::{Consumer, Producer};
+use std::{
+    sync::{atomic::Ordering, Arc},
+    task::Poll,
+};
+
+use rtrb::Consumer;
 
 use crate::{
     handler::process_command,
     kv::KvStore,
-    message::{ResponseMessage, ResponseValue, WorkerMessage},
+    message::{RespFrame, ResponseMessage, WorkerMessage},
+    polling::{notified_ring_buffer::NotifiedProducer, task_notifier::TaskNotifier},
 };
 
-pub async fn worker_main(
-    _worker_id: usize,
-    mut inboxes: Vec<Consumer<WorkerMessage>>,
-    mut resp_outboxes: Vec<Producer<ResponseMessage>>,
-) {
-    let kv = KvStore::new();
+pub struct WorkerTask {
+    worker_id: usize,
+    inboxes: Vec<Consumer<WorkerMessage>>,
+    to_writer: Vec<NotifiedProducer<ResponseMessage>>,
+    doorbell: Arc<TaskNotifier>,
+    kv: KvStore,
+}
 
-    loop {
-        let mut processed = false;
+impl Future for WorkerTask {
+    type Output = ();
 
-        for inbox in inboxes.iter_mut() {
-            if let Ok(msg) = inbox.pop() {
-                let response = match msg.response_value {
-                    ResponseValue::Array(_) => process_command(&kv, msg.response_value),
-                    _ => msg.response_value,
-                };
-                // note: later, we should be using .get() and handling errors properly
-                resp_outboxes[msg.src_core]
-                    .push(ResponseMessage {
-                        seq: msg.seq,
-                        conn_token: msg.conn_token,
-                        response_value: response,
-                    })
-                    .unwrap();
-                processed = true;
+    fn poll(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Self::Output> {
+        let mut did_work = false;
+
+        for i in 0..self.inboxes.len() {
+            let mut quota = 32;
+
+            while quota > 0 {
+                match self.inboxes[i].pop() {
+                    Ok(msg) => {
+                        self.process_message(msg);
+                        did_work = true;
+                    }
+                    Err(_) => break,
+                }
+                quota -= 1;
             }
         }
 
-        if !processed {
-            tokio::task::yield_now().await;
+        // if work is processed, we're hot, yield but do NOT sleep (yield to runtime)
+        if did_work {
+            return Poll::Pending;
         }
+
+        // go to sleep, only wake by manual wakeup
+        self.doorbell.waker.register(cx.waker());
+        self.doorbell.is_sleeping.store(true, Ordering::Release);
+
+        // final check, if anything in inbox, cancel sleep and yield to runtime
+        for consumer in &self.inboxes {
+            if !consumer.is_empty() {
+                self.doorbell.is_sleeping.store(false, Ordering::Release);
+                return Poll::Pending;
+            }
+        }
+
+        Poll::Pending
+    }
+}
+
+impl WorkerTask {
+    pub fn new(
+        worker_id: usize,
+        inboxes: Vec<Consumer<WorkerMessage>>,
+        to_writer: Vec<NotifiedProducer<ResponseMessage>>,
+        doorbell: Arc<TaskNotifier>,
+    ) -> Self {
+        let kv = KvStore::new();
+        WorkerTask {
+            worker_id,
+            inboxes,
+            to_writer,
+            doorbell,
+            kv,
+        }
+    }
+
+    fn process_message(&mut self, msg: WorkerMessage) {
+        let response = match msg.response_value {
+            RespFrame::Array(_) => process_command(&self.kv, msg.response_value),
+            _ => msg.response_value,
+        };
+        // note: later, we should be using .get() and handling errors properly
+        self.to_writer[msg.src_core]
+            .push_with_notify(ResponseMessage {
+                seq: msg.seq,
+                conn_token: msg.conn_token,
+                response_value: response,
+            })
+            .unwrap();
     }
 }
